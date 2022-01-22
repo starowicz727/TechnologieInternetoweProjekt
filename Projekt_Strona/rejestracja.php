0880148c34463a346bdf2ff3743b03d3bd2a4eeb
starowicz727 <starowicz727@gmail.com>
2022-01-22 12:16:28 +0100

    rejestracja skonczona

--- Projekt_Strona/rejestracja.php
+++ Projekt_Strona/rejestracja.php
@@ -1,19 +1,34 @@
 <?php
 if(isset($_POST["re_login"]) && isset($_POST["re_pass"])) //jelsi rejestracja.php nie byl uruchomiony po raz pierwszy
 {
-    if(($_POST["frm_login"]=="a") && ($_POST["frm_pass"]=="a")){ //select*from xyz where login=frm_login and password=frm_pass
-        //echo "udalo sie zalogowac";
-        session_start();
-        $_SESSION["login"]= $_POST["frm_login"];
-        header("Location: logowanie_ok.php"); //tu przechodzimy do kolejnego skryptu
+    require_once("connect.php"); // łączymy się z bazą danych
+
+    $sql = "select * from users where login=?"; //sprawdzamy czy dany login jest zajęty
+    $prep = $conn -> prepare($sql);
+    $prep -> bind_param('s',$_POST['re_login']); 
+    $prep -> execute(); // tu się wykona select
+    $result = $prep -> get_result();
+
+    if($row = $result -> fetch_assoc() != null){ // jeśli select nie zwrócił null => takie konto istnieje=> rejestracja nie powinna się udać
+        echo"Podany login jest już zajęty :(";
     }
-    else{
-        echo "nie udalo sie";
+    else{ // login nie jest zajęty => tworzymy konto 
+        $sql = "insert into users (login, password) values (?,?)";
+        $prep = $conn -> prepare($sql);
+        $hash_pass = sha1($_POST['re_pass']);
+        $prep -> bind_param('ss',$_POST['re_login'], $hash_pass); 
+        $result = $prep -> execute(); // tu się wykona insert
+        if($result){//jeśli się udało
+            echo '<script language="javascript">';
+            echo 'alert("Konto zostało utworzone! Możesz się zalogować")';  //not showing an alert box.
+            echo '</script>';
+            header("Location: logowanie.php"); //tu przechodzimy do kolejnego skryptu
+        }
     }
 }
 
-echo "<form method=post action=logowanie.php>";
-echo "<input name=re_login required=\"required\">Login</br>"; //login moze byc primary key
+echo "<form method=post action=rejestracja.php>";
+echo "<input name=re_login required=\"required\">Login</br>"; 
 echo "<input type=password name=re_pass required=\"required\">Haslo</br>";
 echo "<input type=submit value=Zarejestruj>";
 echo "</form>";
