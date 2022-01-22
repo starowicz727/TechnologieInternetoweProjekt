7ece2bb174d6aba19f606e2128959fa0d4e4e521
PatrycjaPiatek <pi.patrycja99@gmail.com>
2022-01-22 12:59:31 +0100

    bootstrap1

--- Projekt_Strona/logowanie_ok.php
+++ Projekt_Strona/logowanie_ok.php
@@ -1,3 +1,97 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<title>Skoki narciarskie</title>
+	<meta name="description" content="Blog o skokach narciarskich">
+	<meta name="keywords" content="skoki narciarskie, skoczkowie, wyniki">
+	<meta name="author" content="Jan Kowalski">
+	<meta http-equiv="X-Ua-Compatible" content="IE=edge">
+	
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="main.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+	
+	<!--[if lt IE 9]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<![endif]-->
+	
+</head>
+
+<body>
+
+	<header>
+	
+		<nav class="navbar navbar-dark bg-secondary">
+		
+			<a class="navbar-brand" href="#"><img src="img/logo.png" width="30" height="30" class="d-inline-block mr-1 align-bottom" alt=""> Fiszki.pl</a>
+		
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Przełącznik nawigacji">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+		
+			<div class="collapse navbar-collapse" id="mainmenu">
+			
+				<ul class="navbar-nav mr-auto">
+				
+					<li class="nav-item active">
+						<a class="nav-link" href="#"> Start </a>
+					</li>
+					
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false" id="submenu" aria-haspopup="true"> Zawody </a>
+						
+						<div class="dropdown-menu" aria-labelledby="submenu">
+						
+							<a class="dropdown-item" href="#"> Terminarz zawodów </a>
+							<a class="dropdown-item" href="#"> Ranking Pucharu Świata </a>
+							
+							<div class="dropdown-divider"></div>
+							
+							<a class="dropdown-item" href="#"> Sylwetki zawodników </a>
+							<a class="dropdown-item" href="#"> Skocznie narciarskie </a>
+						
+						</div>
+						
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link" href="#"> Historia </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link" href="#"> Zdjęcia </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#"> Wywiady </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link" href="#"> Kontakt </a>
+					</li>
+				
+				</ul>
+			
+				<form class="form-inline">
+				
+					<input class="form-control mr-1" type="search" placeholder="Wyszukaj" aria-label="Wyszukaj">
+					<button class="btn btn-light" type="submit">Znajdź</button>
+				
+				</form>
+			
+			</div>
+		
+		</nav>
+	
+	</header>
+
+</body>
+</html>
+
 <?php
 
 session_start();
