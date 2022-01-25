79a99849fc287f78e0817efaf7924de3b416d62e
Rider <38857907+Hexman7@users.noreply.github.com>
2022-01-25 21:13:13 +0100

    xD

--- Projekt_Strona/logowanie.php
+++ Projekt_Strona/logowanie.php
@@ -1,3 +1,28 @@
+
+<?php
+if(isset($_POST["frm_login"]) && isset($_POST["frm_pass"])) //jeślii logowanie.php nie byl uruchomiony po raz pierwszy
+{
+    require_once("connect.php"); // łączymy się z bazą danych
+
+    $sql = "select * from users where login=? and password=?";
+    $prep = $conn -> prepare($sql);
+    $hash_pass = sha1($_POST['frm_pass']);
+    $prep -> bind_param('ss',$_POST['frm_login'], $hash_pass); 
+    $prep -> execute(); // tu się wykona select
+    $result = $prep -> get_result();
+
+    if($row = $result -> fetch_assoc() != null){ // jeśli select nie zwrócił null => takie konto istnieje=> logowanie powinno się udać
+        session_start();
+        $_SESSION["login"]= $_POST["frm_login"];
+        header("Location: logowanie_ok.php"); //tu przechodzimy do kolejnego skryptu
+    }
+    else{
+        echo "<br><br><br><br>";
+        echo "<h1 class=\"text-center text-danger\">Invalid username or password</h1>";        
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -99,29 +124,7 @@
         </div>
     </fieldset> -->
 
-<?php
-if(isset($_POST["frm_login"]) && isset($_POST["frm_pass"])) //jeślii logowanie.php nie byl uruchomiony po raz pierwszy
-{
-    require_once("connect.php"); // łączymy się z bazą danych
 
-    $sql = "select * from users where login=? and password=?";
-    $prep = $conn -> prepare($sql);
-    $hash_pass = sha1($_POST['frm_pass']);
-    $prep -> bind_param('ss',$_POST['frm_login'], $hash_pass); 
-    $prep -> execute(); // tu się wykona select
-    $result = $prep -> get_result();
-
-    if($row = $result -> fetch_assoc() != null){ // jeśli select nie zwrócił null => takie konto istnieje=> logowanie powinno się udać
-        session_start();
-        $_SESSION["login"]= $_POST["frm_login"];
-        header("Location: logowanie_ok.php"); //tu przechodzimy do kolejnego skryptu
-    }
-    else{
-        echo "<br><br><br><br>";
-        echo "<h1 class=\"text-center text-danger\">Invalid username or password</h1>";        
-    }
-}
-?>
 	<!-- Option 1: Bootstrap Bundle with Popper-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	
