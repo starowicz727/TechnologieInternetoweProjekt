e4dcf400721afc7f7961d5ed6976d162387c7919
Rider <38857907+Hexman7@users.noreply.github.com>
2022-01-25 19:34:43 +0100

    usuwanie konta zapytania...

--- Projekt_Strona/usuwanie_konta_z_bazy.php
+++ Projekt_Strona/usuwanie_konta_z_bazy.php
@@ -108,9 +108,33 @@ if(!isset($_SESSION["login"])){ //jesli użytkownik sie nie zalogował
 }
 else{
 	require_once("connect.php"); // łączymy się z bazą danych
+	$users_login = $_SESSION["login"];
+	
+	$query = "DELETE FROM flashcards WHERE flashcards.category_id IN (SELECT id FROM categories where user_login like ?)";
+	$prep = $conn -> prepare($query);
+    $prep -> bind_param('s',$users_login); 
+    $prep -> execute(); // tu się wykona select
+	
+	
+	$query = "DELETE FROM categories WHERE categories.user_login = ?"; 
+	$prep = $conn -> prepare($query);
+    $prep -> bind_param('s',$users_login); 
+    $prep -> execute(); // tu się wykona select
+	
+	$query = "DELETE FROM users WHERE login = ?";
+	$prep = $conn -> prepare($query);
+    $prep -> bind_param('s',$users_login); 
+    $result = $prep -> execute(); // tu się wykona select
 
+	
+	
+	if ($result){//jeśli się udało
+		session_destroy();
+        header("Location: logowanie_ok.php"); //tu przechodzimy do kolejnego skryptu
+    }else{
+        echo "something went wrong";
+    }
 
-
-}
+   }
 
 ?>
